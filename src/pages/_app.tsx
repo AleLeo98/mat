7cdea1f757a07995f03a795edc1bc2e3d06763fd
Aleleo98 <leonca@usi.ch>
2024-06-17 13:04:37 +0200

    HotjarV2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,38 @@
 // pages/_app.tsx
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Hotjar from '@hotjar/browser';
 import { LanguageProvider } from '../context/LanguageContext'; // Ensure the path is correct
 import { SelectionsProvider } from '../context/SelectionsContext'; // Ensure the path is correct
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-const siteId = 5026640;
-const hotjarVersion = 6;
-
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    Hotjar.init(siteId, hotjarVersion);
+    // Reinitialize Hotjar on route changes
   }, [router]);
 
   return (
-    <LanguageProvider>
-      <SelectionsProvider>
-        <Component {...pageProps} />
-      </SelectionsProvider>
-    </LanguageProvider>
+    <>
+      <Head>
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5026640,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        ` }} />
+      </Head>
+      <LanguageProvider>
+        <SelectionsProvider>
+          <Component {...pageProps} />
+        </SelectionsProvider>
+      </LanguageProvider>
+    </>
   );
 }
-
-export default App;
