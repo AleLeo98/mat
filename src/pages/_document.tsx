e6cc12ff0f1de52d604d90e9d6d40a7eb992aba7
Aleleo98 <leonca@usi.ch>
2024-06-17 19:52:04 +0200

    FixBack

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,27 +1,9 @@
-// pages/_document.tsx
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Hotjar Tracking Code for Mat */}
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:5026709,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
