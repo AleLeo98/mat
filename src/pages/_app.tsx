4a3f986bd470e9a3f66490548c3eed2e510f05fe
Aleleo98 <leonca@usi.ch>
2024-06-17 20:06:10 +0200

    HotJarLast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,29 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LanguageProvider } from '../context/LanguageContext'; // Ensure the path is correct
 import { SelectionsProvider } from '../context/SelectionsContext'; // Ensure the path is correct
-import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
       <SelectionsProvider>
         <Component {...pageProps} />
-        <Analytics />
+        <Script
+          id="HotjarAnalytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5026709,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </SelectionsProvider>
     </LanguageProvider>
   );
