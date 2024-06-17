8429f2da8b51e0346a9b94782a4a4e6260d7dad5
Aleleo98 <leonca@usi.ch>
2024-06-17 13:29:01 +0200

    VercelAnalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,16 @@
 // pages/_app.tsx
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 import { LanguageProvider } from '../context/LanguageContext'; // Ensure the path is correct
 import { SelectionsProvider } from '../context/SelectionsContext'; // Ensure the path is correct
-import Hotjar from '@hotjar/browser';
-
-const HJID = 5026709; // Replace with your Hotjar Site ID
-const HJSV = 6; // Replace with your Hotjar Version
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    Hotjar.init(HJID, HJSV);
-  }, []);
-
   return (
     <LanguageProvider>
       <SelectionsProvider>
         <Component {...pageProps} />
+        <Analytics />
       </SelectionsProvider>
     </LanguageProvider>
   );
