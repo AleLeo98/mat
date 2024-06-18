7f507b587b34f0dd4e2a1c120e3bc6648c408d5c
Aleleo98 <leonca@usi.ch>
2024-06-18 17:00:55 +0200

    Hotjarv4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
-// pages/_app.tsx
+// src/pages/_app.tsx
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LanguageProvider } from '../context/LanguageContext'; // Ensure the path is correct
 import { SelectionsProvider } from '../context/SelectionsContext'; // Ensure the path is correct
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
       <SelectionsProvider>
         <Component {...pageProps} />
-        <Analytics />
       </SelectionsProvider>
     </LanguageProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Hotjar Tracking Code for Mat */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5026709,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
